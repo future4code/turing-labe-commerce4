ca6f660d3f588e1cb24aa39d5a46e8176e376809
Kamila Melo <kamilamelo5@hotmail.com>
2020-06-24 14:56:50 -0300

    Exercício de labe-commerce. Colocamos somente os elementos no site.

---
+++ src/components/Filtros.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Filtros extends React.Component{
+    render(){
+        return(
+            <div>
+                <h3>Filtros:</h3>
+                <p>Valor Mínimo:</p>
+                <input/>
+                <p>Valor Máximo:</p>
+                <input/>
+                <p>Busca Produto:</p>
+                <input/>
+            </div>
+        )
+    }
+}
+
+export default Filtros
